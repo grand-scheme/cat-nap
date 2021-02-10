9d9bf50fa5ac47e4495ace81ea630906df0da252
Shannon Grantski <grantski@pm.me>
2021-02-10 15:33:59 -0800

    hide unused tutorial code, import Item
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- src/components/AppChildren/InventoryControl.js
+++ src/components/AppChildren/InventoryControl.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import Item from './InventoryControlChildren/Item';
 
 class InventoryControl extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
-      masterItemList: []
+//       formVisibleOnPage: false,
+//       masterItemList: []
     };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState(prevState => ({
+//       formVisibleOnPage: !prevState.formVisibleOnPage
+//     }));
   }
 
 
   render(){
+  // function InventoryControl(){ // placeholder
     return (
       <>
       <h1>Inventory placeholder</h1>
+      <Item />
       </>
     );
   }
