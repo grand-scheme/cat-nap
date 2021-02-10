d032d29c9a3a21794b09d9d9418ad10eb24ebdc4
Shannon Grantski <grantski@pm.me>
2021-02-10 15:33:24 -0800

    export item, test with hardcode data
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- src/components/AppChildren/InventoryControlChildren/Item.js
+++ src/components/AppChildren/InventoryControlChildren/Item.js
@@ -1,24 +1,56 @@
 import React from "react";
-import ItemDetails from "./ItemChildren/ItemDetail";
-import PropTypes from "prop-types";
+import ItemDetail from "./ItemChildren/ItemDetail";
+// import PropTypes from "prop-types";
 
+const itemArray = [ // placeholder dummy data
+  {
+    name: 'name',
+    price: 'price',
+    availability: 'availability',
+    size: 'size',
+    description: 'description',
+    quantity: 'quantity'
+  },
+  {
+    name: 'name2',
+    price: 'price2',
+    availability: 'availability2',
+    size: 'size2',
+    description: 'description2',
+    quantity: 'quantity2'
+  },
+  {
+    name: 'name3',
+    price: 'price3',
+    availability: 'availability3',
+    size: 'size3',
+    description: 'description3',
+    quantity: 'quantity3'
+  },
+];
 
-function Item(props) {
+// function Item(props) {
+function Item() {
   return (
     <>
     <hr />
-    {props.item.map((itemdetails, index) =>
-      < ItemDetails
-        name={itemdetails.name}
-        price={itemdetails.price}
-        availability={itemdetails.availability}
-        size={itemdetails.size}
-        description={itemdetails.description}
-        quantity={itemdetails.quantity}
+    {itemArray.map((itemDetail, index) =>
+      <ItemDetail
+        name={itemDetail.name}
+        price={itemDetail.price}
+        availability={itemDetail.availability}
+        size={itemDetail.size}
+        description={itemDetail.description}
+        quantity={itemDetail.quantity}
         key={index}
         />
-    )
-  }
-    </>
-  )
+    )}
+  </>
+  );
 }
+//this is an array with the properties above
+// Item.propTypes = {
+//   itemDetail: PropTypes.array
+// };
+
+export default Item;
